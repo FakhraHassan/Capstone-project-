9f808b35fdb58f69338a9ed67b1567a0e286980c
FakhraHassan <fakhrahassan23@gmail.com>
2026-02-16 19:42:30 +0500

    Update database configuration to MySQL and verified local execution

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -94,8 +94,15 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'capstone_db',
+        'USER': 'root',
+        'PASSWORD': 'fahapak23',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
 
